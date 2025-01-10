72aacce7cb7aafe5f6c826d5fe7c3d85c00bfbce
DavidOlmos03 <juanda_201001@hotmail.com>
2025-01-10 08:11:25 -0500

    PEP 8 implemented in validate_fields function

--- app/schemas.py
+++ app/schemas.py
@@ -8,6 +8,23 @@ class IrisFeatures(BaseModel):
 
     @model_validator(mode="before")
     def validate_fields(cls, values: dict[str, float]) -> dict[str, float]:
+        """
+        Validates the fields of the IrisFeatures class.
+
+        This validator checks that all fields are not empty, are floats or integers, 
+        and are positive.
+        If any of these conditions are not met, it raises a ValueError.
+
+        Args:
+            values (dict[str, float]): A dictionary containing the values of the IrisFeatures class.
+
+        Returns:
+            dict[str, float]: The validated dictionary.
+
+        Raises:
+            ValueError: If any of the fields are empty, not floats or integers, or not positive.
+            TypeError: If any of the fields are not floats or integers.
+        """
         for field in ["sepal_length", "sepal_width", "petal_length", "petal_width"]:
             value = values.get(field)
             if value is None or value == "":
